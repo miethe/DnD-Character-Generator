2d4c1daf0f573e071f25dc0e11fbbfd9242f2fb8
Nick Miethe <6800980+miethe@users.noreply.github.com>
2019-03-05 17:22:27 -0500

    Create dice_roller.py
    A class for rolling any given size die n number of times, as well as ability to drop lowest in a n size set, and drop lowest of a set of sets.

---
+++ dice_roller.py
@@ -0,0 +1,24 @@
+import random
+
+class Dice_Roller:
+
+    def roll_n_d_x(self, count, die_size):
+        rolls = []
+        for _ in range(count):
+            rolls.append(random.randrange(1,die_size))
+        return rolls
+
+    def drop_low_and_tally(self, rolls):
+        rolls.sort()
+        return sum(rolls[1:])
+
+    def roll_ndx_y_times_drop_lowest(self, times_rolled, die_size, total_iterations):
+        total_rolls = []
+        for _ in range(total_iterations):
+            rolls = self.roll_n_d_x(times_rolled, die_size)
+            total_rolls.append(self.drop_low_and_tally(rolls))
+        total_rolls.sort()
+        return total_rolls[1:]
+
+if __name__ == "__main__":
+    print(Dice_Roller().roll_ndx_y_times_drop_lowest(4, 6, 7))
