4267a7fa613be0772a006e1d81417d6924382140
Nick Miethe <nick.miethe@gmail.com>
2019-03-05 19:59:31 -0500

    Adding checks for 0 input

--- dice_roller.py
+++ dice_roller.py
@@ -9,11 +9,15 @@ class Dice_Roller:
         return rolls
 
     def drop_low_and_tally(self, rolls):
+        if not rolls:
+            return rolls
         rolls.sort()
         return sum(rolls[1:])
 
     def roll_ndx_y_times_drop_lowest(self, times_rolled, die_size, total_iterations):
         total_rolls = []
+        if times_rolled is 0 or die_size is 0 or total_iterations is 0:
+            return total_rolls
         for _ in range(total_iterations):
             rolls = self.roll_n_d_x(times_rolled, die_size)
             total_rolls.append(self.drop_low_and_tally(rolls))
