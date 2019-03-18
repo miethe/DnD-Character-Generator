7454a23c0d268c69a9e7a139246afc69e0712869
Nick Miethe <nick.miethe@gmail.com>
2019-03-17 22:03:19 -0400

    Another massive overhaul of architecture. Character is now not a Race

--- dnd_races.py
+++ dnd_races.py
@@ -3,6 +3,7 @@ import sys
 import time
 import argparse
 import csv
+from copy import deepcopy
 
 from dice_roller import Dice_Roller
 
@@ -26,19 +27,19 @@ STAT_ABBREVIATIONS = {'str':'strength', 'dex':'dexterity', 'con':'constitution',
 
 class Race(object):
 
-    def __init__(self, race_name = 'Aasimar', stats = {}, info = 'None', size = 0, speed = 30, language = 'Common', source = 'PHB'):
+    def __init__(self, race_name = 'Aasimar', subrace_name='Protector', stats = {}, info = 'None', size = 0, speed = 30, language = 'Common', source = 'PHB'):
         self.race_name = race_name
-        self.subraces = []
-        self.stats = stats
-        if stats:
-            self.set_stats(stats)
-        else:
-            self.set_stats(stats)
+        self.subrace_name = subrace_name
+        self.stats = {}
         self.size = size
         self.speed = speed
         self.language = language
         self.race_info = info
         self.source = source
+        if stats:
+            self.set_stats(stats)
+        else:
+            self.set_stats(stats)
 
     def strength(self):
         self.stats['strength']
@@ -58,75 +59,45 @@ class Race(object):
     def charisma(self):
         self.stats['charisma']
 
-    def add_subrace(self, subrace, stats, info, size, speed, language, source):
-        self.subraces.append(Subrace(self.race_name, subrace, stats, info, size, speed, language, source))
+    def get_race_name(self):
+        return ('{0}: {1}'.format(self.race_name, self.subrace_name))
 
-    def get_stats(self, subrace = None):
-        if not subrace:
-            subrace = self.subraces[0]
-        return subrace.get_stats()
+    def get_language(self):
+        return self.language
+
+    def get_stats(self):
+        return self.stats
 
     def set_stats(self, stats):
-        if stats and stats is list:
+        if stats and type(stats) is list:
             self.stats['strength'] = stats[0]
             self.stats['dexterity'] = stats[1]
             self.stats['constitution'] = stats[2]
             self.stats['intelligence'] = stats[3]
             self.stats['wisdom'] = stats[4]
             self.stats['charisma'] = stats[5]
-        else:
-            self.stats = stats
-
-class Subrace(Race):
-
-    def __init__(self, race, subrace = 'None', stats = {}, info = 'None', size = 0, speed = 30, language = 'Common', source = 'PHB'):
-        Race.__init__(self, race, stats, info, size, speed, language, source)
-        self.subrace_name = subrace
-        self.race = race
-
-    def get_stats(self):
-        return self.stats
 
 class Character(object):
 
-    def __init__(self, subrace = Subrace('Aasimar', 'None'), name = 'None', gender = 'Male'):
-        self.subrace = subrace
+    def __init__(self, race, name = 'None', gender = 'Male'):
+        self.race = deepcopy(race)
         self.name = name
-        self.stats = subrace.get_stats()
         self.gender = gender
-        self.language = subrace.language
 
-    def strength(self):
-        self.stats['strength']
-
-    def dexterity(self):
-        self.stats['dexterity']
-
-    def constitution(self):
-        self.stats['constitution']
-
-    def intelligence(self):
-        self.stats['intelligence']
-
-    def wisdom(self):
-        self.stats['wisdom']
-
-    def charisma(self):
-        self.stats['charisma']
-
-    def set_stats(self, stats, list = True):
-        if list and stats:
-            self.stats['strength'] = stats[0]
-            self.stats['dexterity'] = stats[1]
-            self.stats['constitution'] = stats[2]
-            self.stats['intelligence'] = stats[3]
-            self.stats['wisdom'] = stats[4]
-            self.stats['charisma'] = stats[5]
-        else:
-            self.stats = stats
+    def get_language(self):
+        return self.race.language
 
     def get_stats(self):
-        return self.stats
+        return self.race.stats
+
+    def get_race_name(self):
+        return ('{0}: {1}'.format(self.race.race_name, self.race.subrace_name))
+
+def populate_races(desired_race=None, sub_race = '(none)', return_subraces = False):
+    important_info={'first_row':True, 'race':desired_race, 'subrace':sub_race, 'undesirables':['cent/mino', 'ravnica', 'psz', 'eberron']}
+    filepath = 'races.csv'
+    
+    return parse_csv_into_dict(filepath, important_info)
 
 def parse_csv_into_dict(filepath, important_info):
     output_dict = {}
@@ -136,29 +107,30 @@ def parse_csv_into_dict(filepath, important_info):
             process_row_as_race(row, output_dict, important_info)
     return output_dict
 
-def populate_races(desired_race=None, sub_race = '(none)', return_subraces = False):
-    important_info={'first_row':True, 'race':desired_race, 'subrace':sub_race, 'undesirables':['cent/mino', 'ravnica', 'psz', 'eberron']}
-    filepath = 'races.csv'
-    
-    return parse_csv_into_dict(filepath, important_info)
-
 def process_row_as_race(row, races, important_info):
     if important_info['first_row']:
         important_info['first_row'] = False
         return
-    race_name = row[RACE]
-    if race_name not in races:
-        races[race_name] = Race(race_name)
-    
-    race = races[race_name]
-    populate_race_info(race, row)
 
     if 'choose one' in row[SUBRACE].lower():
         return
-    else:
-        create_new_subrace(row, race)
-    return
+    if 'none' in row[SUBRACE].lower():
+        row[SUBRACE] = 'None'
+    
+    race = add_race_to_dict_and_return(row, races)
 
+    populate_race_info(race, row)
+
+def add_race_to_dict_and_return(row, races):
+    race_name = row[RACE]
+    subrace_name = row[SUBRACE]
+    dict_race_name = '{0}:{1}'.format(race_name, subrace_name)
+
+    if dict_race_name not in races:
+        races[dict_race_name] = Race(race_name, subrace_name)
+
+    return races[dict_race_name]
+    
 def populate_race_info(race, row):
     race.language = row[LANGUAGE]
     race.race_info = row[EX_INFO]
@@ -166,7 +138,10 @@ def populate_race_info(race, row):
     race.size = row[SIZE]
     race.speed = row[SPEED]
 
-def get_stats(row):
+    stats = retrieve_stats(row)
+    race.set_stats(stats)
+
+def retrieve_stats(row):
     index = STR
     stats = []
 
@@ -178,43 +153,9 @@ def get_stats(row):
         index += 1
     return stats
 
-def create_new_subrace(row, race):
-    subrace = row[SUBRACE]
-    size = row[SIZE]
-    speed = row[SPEED]
-    lang = row[LANGUAGE]
-    info = row[EX_INFO]
-    source = row[SOURCE]
-
-    stats = get_stats(row)
-    
-    race.add_subrace(subrace, stats, info, size, speed, lang, source)
-
-def skip_if_filtered(row, important_info):
-    if important_info['first_row']:
-        important_info['first_row'] = False
-        return True
-    if len(row) <=1:
-        return True
-    if row[SOURCE].lower().split(' ')[0] in important_info['undesirables']:
-        return True
-    if 'choose one' in row[SUBRACE].lower():
-        return True
-
-    if important_info['race'] is None:
-        return False
-    elif important_info['race'].lower() not in row[RACE].lower():
-        return True
-
-    if important_info['subrace'] is 'all':
-        return False
-    elif important_info['subrace'].lower() not in row[SUBRACE].lower():
-        return True
-    return False
-
 def add_rolled_choice_to_race_stats(choice, number, character):
     stat = STAT_ABBREVIATIONS[choice.lower()]
-    character.stats[stat] += number
+    character.get_stats()[stat] += number
 
 """ def generate(race='', number=1, gender=''):
     mpath = './models/rnn_layer_epoch_250.pt'
@@ -228,18 +169,15 @@ def add_rolled_choice_to_race_stats(choice, number, character):
 if __name__ == '__main__':
     #generate()
     races = populate_races()
-    new_character = Character(races['Aasimar'])
+
+    new_character = Character(races['Aasimar:Protector'])
     auto_assign = True
 
     rolled_stats = Dice_Roller().roll_ndx_y_times(4, 6, 7, True)
 
-    #race_stats = new_character.get_stats()
-    #new_character.subrace.set_stats(race_stats, True)
+    print(str(new_character.get_race_name()))
 
-    print(str(new_character.subrace.race_name))
-    print(str(new_character.language))
-
-    print('Current Stats: ' + str(new_character.stats))
+    print('Current Stats: ' + str(new_character.get_stats()))
     print('Rolled numbers: ' + str(rolled_stats))
 
     stats_assigned = {stat:0 for stat in STAT_NAMES}
@@ -264,4 +202,4 @@ if __name__ == '__main__':
 
         add_rolled_choice_to_race_stats(choice, number, new_character)
 
-    print(str(new_character.stats))
+    print(str(new_character.get_stats()))
